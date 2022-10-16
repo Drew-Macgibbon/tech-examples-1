88950b562d5667c0a82f2e55e9952e8d41e26e69
Drew MacGibbon <drewmacgibbon@gmail.com>
2022-10-16 15:02:21 +0530

    ADD: Storybook/vue working

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,5 +16,11 @@ export default defineConfig({
   output: "server",
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  vite: {
+    ssr: {
+      // Example: Force a broken package to skip SSR processing, if needed
+      external: ['broken-npm-package'],
+    }
+  }
 });
